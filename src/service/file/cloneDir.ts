401dd6219fcd356863a3dafa077c9b2dd982beb8
Jens Brimfors <jens.brimfors@qapital.com>
2022-10-26 13:30:56 +0200

    Implement some basic repo analysis

--- src/service/file/cloneDir.ts
+++ src/service/file/cloneDir.ts
@@ -2,7 +2,8 @@ import {fs} from "@tauri-apps/api";
 import {MegaSettingsType} from "../../hooks/MegaContext";
 import {asString} from "../../hooks/logWrapper";
 import {debug, error, trace, warn} from "tauri-plugin-log-api";
-import {Command} from "@tauri-apps/api/shell";
+import {ChildProcess, Command} from "@tauri-apps/api/shell";
+import {CloneWorkMeta} from "../git/cloneWorker";
 
 export async function listClones(settings: MegaSettingsType): Promise<string[]> {
   trace('listClones')
@@ -59,19 +60,41 @@ export async function analyzeRepoForBadStates(settings: MegaSettingsType, repoPa
 }
 
 async function hasUncommittedChanges(repoPath: string): Promise<boolean> {
-  // todo: impl
-  // git diff HEAD
   const result = await new Command('git', ['diff'], {cwd: repoPath}).execute()
   debug(`Ran 'git diff' in ${repoPath} with result: ${asString(result)}`)
   return result.code !== 0 || result.stdout.length !== 0 || result.stderr.length !== 0
 }
 
 async function hasOnDefaultBranch(repoPath: string): Promise<boolean> {
-  // todo: impl
-  return true;
+  const [current, main] = await Promise.all([
+    getCurrentBranchName(repoPath),
+    getMainBranchName(repoPath),
+  ])
+  return current === main;
 }
 
 async function hasNoDiffWithOriginHead(repoPath: string): Promise<boolean> {
-  // todo: impl
-  return true;
+  const [current, main] = await Promise.all([
+    getCurrentBranchName(repoPath),
+    getMainBranchName(repoPath),
+  ])
+  const diffResult = await new Command('git', ['diff', current, `origin/${main}`]).execute()
+  return diffResult.code !== 0 || diffResult.stdout !== '' || diffResult.stderr !== ''
+}
+
+async function getCurrentBranchName(repoDir:string): Promise<string>{
+  const result: ChildProcess = await new Command('git', ['branch'], {cwd:repoDir}).execute()
+  if (result.code !== 0) throw new Error(`Unable to determine current branch name of ${repoDir} due to ${asString(result)}`)
+  const currentBranchLine: string | undefined = result.stdout.split('\n').find((line) => line.startsWith('* '))
+  if (!currentBranchLine) throw new Error(`Unable to determine current branch name of ${repoDir}, unintelligible output`)
+  return currentBranchLine.substring(2)
+}
+
+async function getMainBranchName(repoDir: string): Promise<string> {
+  const result: ChildProcess = await new Command('git', ['remote', 'show', 'origin'], {cwd:repoDir}).execute()
+  if (result.code !== 0) throw new Error(`Unable to determine head branch name of ${repoDir} due to ${asString(result)}`)
+  const headBranchRow: string | undefined = result.stdout.split('\n').find(e => e.startsWith('  HEAD branch: '))
+  if (!headBranchRow) throw new Error(`Unable to head branch of ${repoDir}`)
+  const rowParts:string[] = headBranchRow.split(' ')
+  return rowParts[rowParts.length - 1]
 }

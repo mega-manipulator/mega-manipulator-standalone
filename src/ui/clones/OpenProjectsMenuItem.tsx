4d9acbbb212cb50b9fd60ac2c63060c3680f4250
Jens Brimfors <jens.brimfors@qapital.com>
2022-11-15 15:19:01 +0100

    Disable open action for Linux and windows, since it's not implemented

--- src/ui/clones/OpenProjectsMenuItem.tsx
+++ src/ui/clones/OpenProjectsMenuItem.tsx
@@ -1,16 +1,26 @@
-import React, {useContext, useState} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import {Box, Button, ListItemButton, Modal, Typography} from "@mui/material";
 import {MegaContext} from "../../hooks/MegaContext";
 import {modalStyle} from "../modal/megaModal";
 import {error} from "tauri-plugin-log-api";
 import {asString} from "../../hooks/logWrapper";
 import {openDirs} from "../../service/file/scriptFile";
+import {os} from "@tauri-apps/api";
 
 export const OpenProjectsMenuItem: React.FC = () => {
   const {clones: {selected}, settings} = useContext(MegaContext);
   const [isOpen, setIsOpen] = useState(false);
+  const [available, setAvailable] = useState(false);
+  useEffect(() => {
+    (async ()=>{
+      if((await os.type()) === "Darwin"){
+        setAvailable(true)
+      }
+    })()
+  }, []);
+
   return <>
-    <ListItemButton onClick={()=>setIsOpen(true)}>Open with EditorApplication</ListItemButton>
+    <ListItemButton disabled={!available} onClick={()=>setIsOpen(true)}>Open with EditorApplication</ListItemButton>
     <Modal open={isOpen} onClose={() => setIsOpen(false)}>
       <Box sx={modalStyle}>
         <Typography>Really Run open {selected.length} with {settings.editorApplication}?</Typography>

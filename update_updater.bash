2641823ac1c93d3a06c44adbb17bc7609cffc5ac
Jens Brimfors <jens.brimfors@qapital.com>
2023-01-03 10:02:08 +0100

    Update the update_updater-script
    Signed-off-by: Jens Brimfors <jens.brimfors@qapital.com>

--- update_updater.bash
+++ update_updater.bash
@@ -19,15 +19,18 @@ function update(){
 }
 releaseName="$(jq -r '.name' "$tmp_file")"
 releaseBody="$(jq -r '.body' "$tmp_file")"
+releaseNotes="$(sed 's|\\n\\nnull$||' <<< "$releaseName\n\n$releaseBody")"
 tagName="$(jq -r '.tagName' "$tmp_file")"
+version="$(sed 's|^v||' <<< "$tagName")"
 createdAt="$(jq -r '.createdAt' "$tmp_file")"
 
 update "windows-x86_64" "_x64_en-US.msi.zip"
 update "darwin-x86_64" ".app.tar.gz"
+update "darwin-aarch64" ".app.tar.gz"
 update "linux-x86_64" "_amd64.AppImage.tar.gz"
 
 newContent="$(jq ".platforms = $(cat $tmp_platforms)" tauri-update.json)"
-newContent="$(jq ".notes = \"$releaseName\n\n$releaseBody\"" <<< "$newContent")"
-newContent="$(jq ".version = \"$tagName\"" <<< "$newContent")"
+newContent="$(jq ".notes = \"$releaseNotes\"" <<< "$newContent")"
+newContent="$(jq ".version = \"$version\"" <<< "$newContent")"
 newContent="$(jq ".pub_date = \"$createdAt\"" <<< "$newContent")"
 echo "$newContent" > tauri-update.json

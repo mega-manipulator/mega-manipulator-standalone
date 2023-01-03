7f93840001a7170d92b3e4bbb56b74b7d809402c
Jens Brimfors <jens.brimfors@qapital.com>
2023-01-03 07:04:35 +0100

    Please the fmt and clippy overlords

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,34 +2,37 @@
 all(not(debug_assertions), target_os = "windows"),
 windows_subsystem = "windows"
 )]
+#![forbid(unsafe_code)]
+#![deny(warnings)]
 
 use std::fs;
+
 use log::LevelFilter;
 use tauri_plugin_log::{LoggerBuilder, LogTarget, RotationStrategy};
 
 #[tauri::command]
 fn store_password(username: String, password: String) -> Result<(), String> {
-    let service = "mega-manipipulator";
-    let entry = keyring::Entry::new(&service, &username);
+    let service: &str = "mega-manipipulator";
+    let entry: keyring::Entry = keyring::Entry::new(service, &username);
 
     match entry.set_password(&password) {
         Ok(p) => Ok(p),
-        Err(e) => Err(format!("Failed setting password. {:?}", e).into()),
+        Err(e) => Err(format!("Failed setting password. {:?}", e)),
     }
 }
 
 #[tauri::command]
 fn get_password(username: String) -> Result<String, String> {
-    let service = "mega-manipipulator";
-    let entry = keyring::Entry::new(&service, &username);
+    let service: &str = "mega-manipipulator";
+    let entry: keyring::Entry = keyring::Entry::new(service, &username);
     match entry.get_password() {
         Ok(p) => Ok(p),
-        Err(e) => Err(format!("Failed getting password. {:?}", e).into()),
+        Err(e) => Err(format!("Failed getting password. {:?}", e)),
     }
 }
 
 #[tauri::command]
-fn copy_dir(source: String, dest:String) -> Result<(), String> {
+fn copy_dir(source: String, dest: String) -> Result<(), String> {
     println!("Copying from {:?} to {:?}", source, dest);
     let _ = fs::create_dir(&dest);
     let mut options = fs_extra::dir::CopyOptions::new();
@@ -50,11 +53,7 @@ fn main() {
         .plugin(
             LoggerBuilder::default()
                 .level(LevelFilter::Debug)
-                .targets([
-                    LogTarget::LogDir,
-                    LogTarget::Stdout,
-                    LogTarget::Webview,
-                ])
+                .targets([LogTarget::LogDir, LogTarget::Stdout, LogTarget::Webview])
                 .rotation_strategy(RotationStrategy::KeepAll)
                 .build(),
         )
@@ -64,7 +63,11 @@ fn main() {
             tauri::Menu::default()
         })
         // This is where you pass in your commands
-        .invoke_handler(tauri::generate_handler![store_password, get_password, copy_dir])
+        .invoke_handler(tauri::generate_handler![
+            store_password,
+            get_password,
+            copy_dir
+        ])
         .run(context)
         .expect("error while running tauri application");
 }

c278adb3e20ddbbc1311af7de67055f205d7cdff
Jens Brimfors <jens.brimfors@qapital.com>
2023-01-03 16:16:19 +0100

    Chunk parallel actions 25 at a time
    Signed-off-by: Jens Brimfors <jens.brimfors@qapital.com>

--- src/service/file/simpleActionWithResult.ts
+++ src/service/file/simpleActionWithResult.ts
@@ -17,7 +17,11 @@ export interface SimpleActionProps {
 /**
  * Run stuff in parallel
  */
-export async function simpleAction<T>(input: SimpleActionProps, action: (index: number, hit: SearchHit, path: string) => Promise<T>, errMapper: (hit: SearchHit, err: unknown) => Promise<T>): Promise<(T)[]> {
+export async function simpleAction<T>(
+  input: SimpleActionProps,
+  action: (index: number, hit: SearchHit, path: string) => Promise<T>,
+  errMapper: (hit: SearchHit, err: unknown) => Promise<T>,
+): Promise<(T)[]> {
   const clonePath = await validClonePath(input.settings)
   const promises: Promise<T>[] = input.hits.map(async (hit, i): Promise<T> => {
     const _hit = await hitToSearchHit(hit);
@@ -29,7 +33,14 @@ export async function simpleAction<T>(input: SimpleActionProps, action: (index: 
       return await errMapper(_hit, e);
     }
   });
-  return await Promise.all(promises);
+  const chunkSize = 25;
+  const aggregate: T[] = [];
+  for (let i = 0; i < promises.length; i += chunkSize) {
+    const chunk = promises.slice(i, i + chunkSize)
+    const awaited: T[] = await Promise.all(chunk);
+    aggregate.push(...awaited)
+  }
+  return aggregate;
 }
 
 async function hitToSearchHit(hit: SearchHit | string): Promise<SearchHit> {
